1d7ae6914a7443304e4ef997970aa7b2c8ef723d
Joao Pedro <joaosvargas83@gmail.com>
2026-01-22 13:15:28 -0300

    aumentando o código

--- TesteGit.py
+++ TesteGit.py
@@ -1,7 +1,13 @@
-nome = input('Qual o seu nome? ')
+nome = input('Qual o seu nome? ').capitalize()
 print(f'Bem vindo, {nome}')
 idade = int(input('Qual a sua idade? '))
 if idade >= 18:
     print('Ok, voçe é maior de idade')
 else:
     print('Voçe ainda é menor de idade')
+print('Desenvolvimento de software é considerado complexo, não exige esforço físico mas bastante mental')
+tempo = input(f'Faz quanto tempo {nome}, que voçe estuda Desenvolvimento de Software? ')
+if '1' and 'ano' in tempo:
+    print('Já faz um bom tempo, se ainda não está trabalhando com isso, logo irá se for da tua vontade')
+elif '1' and 'mes' in tempo:
+    print('Decisão recente, te desejo bons estudos, logo irá poder atuar na área')
